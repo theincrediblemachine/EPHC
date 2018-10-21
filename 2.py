80d55dc24f3848a12de18be931e8473c475d3956
theincrediblemachine <44179656+theincrediblemachine@users.noreply.github.com>
2018-10-21 16:14:57 +0200

    Delete 2.py

--- 2.py
+++
@@ -1,8 +0,0 @@
-# vraag gebruiker om getal, afhankelijk van of het getal even of oneven is, print een bericht terug
-
-i = input("gief number\n")
-x = int(i)/2
-if x == int:
-    print("This is an even number.")
-else:
-    print("This is an odd number.")
